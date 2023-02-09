73206018c7903ec69f693c561f990ae3da6f0783
Thom Wiggers <thom@thomwiggers.nl>
2023-02-09 11:02:44 +0100

    Update algorithm list

--- algorithms.py
+++ algorithms.py
@@ -1,67 +1,50 @@
 """The algorithms in use"""
 import itertools
 
+from typing import Tuple
 
 # for legacy reasons, these are tuples,
 # but both sides should be equal up to case.
 
-signs = [
+signs: list[Tuple[str, str]] = [
     ("dilithium2", "Dilithium2"),
     ("dilithium3", "Dilithium3"),
     ("dilithium5", "Dilithium5"),
     ("falcon512", "Falcon512"),
     ("falcon1024", "Falcon1024"),
-    ("rainbowiclassic", "RainbowIClassic"),
-    ("rainbowicircumzenithal", "RainbowICircumzenithal"),
-    ("rainbowicompressed", "RainbowICompressed"),
-    ("rainbowiiiclassic", "RainbowIiiClassic"),
-    ("rainbowiiicircumzenithal", "RainbowIiiCircumzenithal"),
-    ("rainbowiiicompressed", "RainbowIiiCompressed"),
-    ("rainbowvclassic", "RainbowVClassic"),
-    ("rainbowvcircumzenithal", "RainbowVCircumzenithal"),
-    ("rainbowvcompressed", "RainbowVCompressed"),
-    *[(sphincs.lower(), sphincs) for sphincs in (
-        f"Sphincs{hash}{size}{fs}{kind}"
-        for hash in ("Haraka", "Sha256", "Shake256")
-        for size in ("128", "192", "256")
-        for fs in ("f", "s")
-        for kind in ("Simple", "Robust")
-    )],
+    *[
+        (sphincs.lower(), sphincs)
+        for sphincs in (
+            f"Sphincs{hash}{size}{fs}{kind}"
+            for hash in ("Haraka", "Sha256", "Shake256")
+            for size in ("128", "192", "256")
+            for fs in ("f", "s")
+            for kind in ("Simple", "Robust")
+        )
+    ],
+    *[
+        (f"pqov{size}", f"Pqov{size}")
+        for size in ("1616044", "25611244", "25618472", "25614496")
+    ],
     ("xmss", "XMSS"),
 ]
 
-kems = [
+kems: list[Tuple[str, str]] = [
     ("kyber512", "Kyber512"),
     ("kyber768", "Kyber768"),
     ("kyber1024", "Kyber1024"),
     *[
-        (f"classicmceliece{size}", f"ClassicMcEliece{size}")
+        (f"classicmceliece{size}{variant}", f"ClassicMcEliece{size}{variant}")
         for size in (
             "348864",
-            "348864f",
             "460896",
-            "460896f",
             "6688128",
-            "6688128f",
             "6960119",
-            "6960119f",
             "8192128",
-            "8192128f",
         )
+        for variant in ["", "f"]
     ],
-    ("lightsaber", "Lightsaber"),
-    ("saber", "Saber"),
-    ("firesaber", "Firesaber"),
-    ("ntruhps2048509", "NtruHps2048509"),
-    ("ntruhps2048677", "NtruHps2048677"),
-    ("ntruhps4096821", "NtruHps4096821"),
-    ("ntruhrss701", "NtruHrss701"),
-    ("ntruprimentrulpr653", "NtruPrimeNtrulpr653"),
-    ("ntruprimentrulpr761", "NtruPrimeNtrulpr761"),
-    ("ntruprimentrulpr857", "NtruPrimeNtrulpr857"),
-    ("ntruprimesntrup653",  "NtruPrimeSntrup653"),
-    ("ntruprimesntrup761",  "NtruPrimeSntrup761"),
-    ("ntruprimesntrup857",  "NtruPrimeSntrup857"),
+    ("ntruprimesntrup761", "NtruPrimeSntrup761"),
     *[
         (f"frodokem{size}{alg}", f"FrodoKem{size.title()}{alg.title()}")
         for size in ("640", "976", "1344")
@@ -77,7 +60,7 @@ kems = [
     *[(f"hqc{size}", f"Hqc{size}") for size in ["128", "192", "256"]],
 ]
 
-nikes = [
+nikes: list[str] = [
     "CSIDH2047k221",
     "CSIDH4095k256",
     "CSIDH5119k234",
@@ -93,20 +76,27 @@ nikes = [
 ]
 
 
-oids = {var: i for (i, (var, _)) in enumerate(itertools.chain(signs, kems, zip(map(lambda x: x.lower(), nikes), nikes)), start=1)}
+oids: dict[str, int] = {
+    var: i
+    for (i, (var, _)) in enumerate(
+        itertools.chain(signs, kems, zip(map(lambda x: x.lower(), nikes), nikes)),
+        start=1,
+    )
+}
 
 
-def get_oid(algorithm):
+def get_oid(algorithm: str) -> str:
     oid = oids[algorithm]
     return f"1.3.6.1.4.1.44363.46.{oid}"
 
 
-def get_oqs_id(algorithm):
+def get_oqs_id(algorithm: str) -> str:
     return dict(signs + kems)[algorithm]
 
 
 def is_sigalg(algorithm: str) -> bool:
     return algorithm.lower() in dict(signs).keys()
+
 
 def is_kem(algorithm: str) -> bool:
     return algorithm.lower() in dict(kems).keys()
