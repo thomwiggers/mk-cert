46dac88ed650bc8b358fdc30d01cd119d23e1c25
Thom Wiggers <thom@thomwiggers.nl>
2023-02-21 15:07:40 +0100

    fix csidhutil speedtest

--- csidhutil/examples/speed.rs
+++ csidhutil/examples/speed.rs
@@ -5,13 +5,13 @@ use std::hint::black_box;
 const ITERATIONS: u128 = 100;
 
 fn main() {
-    let (pk, sk) = csidh.keygen();
-    let (pk2, _) = csidh.keygen();
+    let (_pk, sk) = csidh::keygen();
+    let (pk2, _) = csidh::keygen();
 
     let keypair_time = {
         let start = Instant::now();
         for _ in 0..ITERATIONS {
-            let _ = black_box(csidh.keygen());
+            let _ = black_box(csidh::keygen());
         }
         Instant::now().duration_since(start).as_micros() / ITERATIONS
     };
@@ -19,15 +19,15 @@ fn main() {
     let derive_time = {
         let start = Instant::now();
         for _ in 0..ITERATIONS {
-            black_box(csidh.derive(&pk2, black_box(&sk))).unwrap();
+            black_box(csidh::derive(&pk2, black_box(&sk)));
         }
         Instant::now().duration_since(start).as_micros() / ITERATIONS
     };
 
 
     println!(
-        "{},{},{},{}",
-        csidh.name(),
+        "{},{},{}",
+        csidh::name(),
         keypair_time,
         derive_time,
     );
